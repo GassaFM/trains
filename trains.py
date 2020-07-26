69006d3ea6bf748ce2b0d1c1c1974f9fa12b9121
Ivan Kazmenko <gassa@mail.ru>
2020-07-27 00:44:38 +0300

    stage 4

--- trains.py
+++ trains.py
@@ -5,6 +5,8 @@ import pygame
 backColor = (0, 127, 0)
 railColor = (191, 191, 191)
 tieColor = (160, 82, 45)
+carColor = [(175, 105, 238), (191, 63, 63), (63, 191, 63), (63, 63, 191),
+    (127, 127, 63), (127, 63, 127), (63, 127, 127)]
 
 displayW = 800
 displayH = 300
@@ -70,6 +72,8 @@ for row in range (rows):
 			board[row][col] |= TileKind.L | TileKind.R | TileKind.RD
 		elif cur == 'd':
 			board[row][col] |= TileKind.L | TileKind.R | TileKind.RU
+		else:
+			assert (False)
 
 pygame.init ()
 
@@ -109,6 +113,10 @@ def drawRail2 (x, y):
 	pygame.draw.lines (layer[1], railColor, False,
 	    [(x, y + 15 - 11), (x + 29, y + 15 + 0)], 2)
 
+def drawGoal (x, y, c):
+	pygame.draw.rect (layer[2], carColor[c],
+	    (x + 5, y + 19, 49, 4))
+
 def drawTile (x, y, kind):
 	if kind & TileKind.L:
 		drawRail0 (x + 0, y + 0)
@@ -122,6 +130,8 @@ def drawTile (x, y, kind):
 		drawRail2 (x + 0, y + 0)
 	if kind & TileKind.RD:
 		drawRail2 (x + 30, y + 12)
+	if kind & (TileKind.GOAL * 7):
+		drawGoal (x, y, kind & 7)
 
 def draw ():
 	layer[2].fill (backColor)
